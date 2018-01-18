5d9f2dc6e6838a296021c573b212657e99e146a6
samgzl <samanthagunzl44@gmail.com>
2018-01-18 13:38:55 -0800

    quicksort needs work

--- Program1/Sorts.java
+++ Program1/Sorts.java
@@ -65,6 +65,61 @@ public class Sorts {
 		 }
 	 }
 	
-	
+	 public static void quickSort (int[] arr, int N) {
+		 quickSort(arr, 0, N-1);	 
+	 }
+	 
+	 private static void quickSort (int[] list, int first, int last) {
+		 if (first < last) {
+			 setPivotToEnd(list, first, last);
+			 int pivotIndex = splitList(list, first, last);
+			 quickSort(list, first, pivotIndex-1);
+			 quickSort(list, pivotIndex+1, last);
+		 } 
+		 
+	 }
+	 
+	 private static void setPivotToEnd (int[] arr, int left, int right) {
+		 int first = arr[left];
+		 int last = arr[right];
+		 int middle = arr[(left+right)/2];
+		 int min = Math.min(first, Math.min(middle, last));
+		 int max = Math.max(first, Math.max(middle, last));
+		 int median;
+		 if ((min == first && max == last) || (max == first && min == last)) {
+			 median = middle;
+		 }
+		 else if ((min == middle && max == last) || (min == last && max == middle)) {
+			 median = first;
+		 } 
+		 else {
+			 median = last;
+		 } 
+		 arr[left] = min;
+		 arr[(left+right)/2] = max;
+		 arr[right] = median;
+	 }
+	 
+	 private static int splitList (int[] arr, int left, int right) {
+		 int indexL = left;
+		 int indexR = right-1;
+		 while (indexL < indexR) {
+			 while (arr[indexL] < arr[right]) {
+				 indexL++;
+			 }
+			 while (indexL < indexR && arr[indexR] >= arr[right]) {
+				 indexR--;
+			 }
+			 if (indexL < indexR) {
+				 int temp = arr[indexR];
+				 arr[indexR] = arr[indexL];
+				 arr[indexL] = temp;
+			 }
+		 }
+		 int temp = arr[indexL];
+		 arr[indexL] = arr[right]; //swap with pivot
+		 arr[right] = temp;
+		 return indexL; //new pivot location
+	 }
 	
 }
