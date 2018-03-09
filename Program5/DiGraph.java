9fae6d2341726b414a534f0aa54457ef34e01005
samgzl <samanthagunzl44@gmail.com>
2018-03-09 14:15:11 -0800

    Update DiGraph.java

--- Program5/DiGraph.java
+++ Program5/DiGraph.java
@@ -1,9 +1,75 @@
+// Samantha Gunzl, Quinn Coleman
+// sgunzl, qcoleman
+// Program 5 
+// 3/14/18
+
 import java.util.LinkedList;
 
 import java.util.Queue;
 
 public class DiGraph {
+	
+	private class VertexInfo {
+		
+		// natural indexing
+		private int distance;
+		private int predecessor;
+		
+		public VertexInfo(int distance, int predecessor) {
+			this.distance = distance;
+			this.predecessor = predecessor;	
+		}
+	}
+	
+	
 	private LinkedList<Integer>[] linkedListArray;
+	
+	private VertexInfo[] BFS(int s) {
+		int N = linkedListArray.length;
+		VertexInfo[] VA = new VertexInfo[N+1];
+		for (int u=1; u<=N; u++) {
+			VA[u].distance = -1;
+			VA[u].predecessor = -1;
+		}
+		VA[s].distance = 0;
+		Queue queue = new LinkedList<Integer>();
+		queue.add(s);
+		while (!queue.isEmpty()) {
+			int u = (int) (queue.remove());
+			for (int v=0; v<linkedListArray[u].size(); v++) { // for u's each adjacent vertex v
+				if (VA[v+1].distance == -1) { // v not discovered yet
+					VA[v+1].distance = VA[u].distance + 1;
+					VA[v+1].predecessor = u;
+					queue.add(v+1);
+				}				
+			}
+
+		}
+		return VA;
+	
+	}
+	
+	
+	public boolean isTherePath(int from, int to) {
+		VertexInfo[] VA = BFS(from);
+		if (VA[to].predecessor == -1) {
+			return false;
+		}
+		return true;
+		
+	}
+	
+	public int lengthOfPath(int from, int to) {
+		//call BFS, use data from array
+		VertexInfo[] VA = BFS(from);
+		return VA[to].distance;
+	}
+	
+	public void printPath(int from, int to) {
+		VertexInfo[] VA = BFS(from);
+		
+		
+	}
 	
 	public DiGraph(int n) {
 		linkedListArray = new LinkedList[n];
@@ -11,12 +77,8 @@ public class DiGraph {
 			linkedListArray[i] = new LinkedList<Integer>();
 		}
 	}
-	
-	// Ask - Validity check for vertex in graph?
-	// Ask - can we return value?
-	// Natural indexing?
+
 	public boolean addEdge(int from, int to) {
-		//if (linkedListArray[from-1] != null) {
 		if (linkedListArray[from-1].contains(to)) {
 			return false;
 		}
@@ -57,16 +119,12 @@ public class DiGraph {
 	private int[] indegrees() {
 		int n = linkedListArray.length;
 		int[] indegrees = new int[n];
-		//System.out.println("indegrees: ");
 		for (int u = 0; u < n; u++) { // loop through vertex array
 			for (int v = 0; v < linkedListArray[u].size(); v++) { // loop through each linked list
 				indegrees[linkedListArray[u].get(v) - 1] += 1;
 			}
 		}
-		/*for (int i = 0; i < n; i++) {
-			System.out.print(indegrees[i]);
-		}
-		System.out.println();*/
+
 		return indegrees;
 	}
 	
@@ -82,7 +140,6 @@ public class DiGraph {
 		}
 		int i = 0;
 		while (!q.isEmpty()) {
-			//System.out.println("i: " + i);
 			int u = q.remove();
 			a[i] = u;
 			i++;
@@ -93,7 +150,6 @@ public class DiGraph {
 				}
 			}	
 		}
-		//System.out.println("i: " + i);
 		if (i != n) {
 			throw new IllegalArgumentException();
 		}
