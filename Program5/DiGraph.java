73801e255ee6f0f6f79aef7349ca4263e7371fee
QColeman97 <quinnclmn15@gmail.com>
2018-03-09 12:37:20 -0800

    Program 5 part1,2 finished

--- Program5/DiGraph.java
+++ Program5/DiGraph.java
@@ -1,21 +1,32 @@
 import java.util.LinkedList;
 
+import java.util.Queue;
+
 public class DiGraph {
 	private LinkedList<Integer>[] linkedListArray;
 	
 	public DiGraph(int n) {
 		linkedListArray = new LinkedList[n];
+		for (int i = 0; i < n; i++) {
+			linkedListArray[i] = new LinkedList<Integer>();
+		}
 	}
 	
-	public void addEdge(int from, int to) {
-		if (linkedListArray[from-1].contains(to-1)) {
-			return;
+	// Ask - Validity check for vertex in graph?
+	// Ask - can we return value?
+	// Natural indexing?
+	public boolean addEdge(int from, int to) {
+		//if (linkedListArray[from-1] != null) {
+		if (linkedListArray[from-1].contains(to)) {
+			return false;
 		}
-		linkedListArray[from-1].add(to-1);
+		linkedListArray[from-1].add(to);
+		return true;
 	}
 	
 	public void deleteEdge(int from, int to) {
-		linkedListArray[from-1].remove(to-1);
+		int index = linkedListArray[from-1].indexOf(to);
+		linkedListArray[from-1].remove(index);
 	}
 	
 	public int edgeCount() {
@@ -34,13 +45,59 @@ public class DiGraph {
 		for (int i = 0; i < linkedListArray.length; i++) {
 			System.out.print((i+1) + " is connected to: ");
 			for (int j = 0; j < linkedListArray[i].size(); j++) {
-				System.out.print(linkedListArray[i].get(j) + 1);
+				System.out.print(linkedListArray[i].get(j));
 				if (j != linkedListArray[i].size() - 1) {
 					System.out.print(", ");
 				}
 			}
 			System.out.println();
 		}
+	}
+	
+	private int[] indegrees() {
+		int n = linkedListArray.length;
+		int[] indegrees = new int[n];
+		//System.out.println("indegrees: ");
+		for (int u = 0; u < n; u++) { // loop through vertex array
+			for (int v = 0; v < linkedListArray[u].size(); v++) { // loop through each linked list
+				indegrees[linkedListArray[u].get(v) - 1] += 1;
+			}
+		}
+		/*for (int i = 0; i < n; i++) {
+			System.out.print(indegrees[i]);
+		}
+		System.out.println();*/
+		return indegrees;
+	}
+	
+	public int[] topSort() throws IllegalArgumentException {
+		int n = linkedListArray.length;
+		int []indegrees = indegrees();
+		int[] a = new int[n];
+		Queue<Integer> q = new LinkedList<Integer>();
+		for (int u = 0; u < n; u++) {
+			if (indegrees[u] == 0) {
+				q.add(u+1);
+			}
+		}
+		int i = 0;
+		while (!q.isEmpty()) {
+			//System.out.println("i: " + i);
+			int u = q.remove();
+			a[i] = u;
+			i++;
+			for (int v = 0; v < linkedListArray[u-1].size(); v++) {
+				indegrees[linkedListArray[u-1].get(v) - 1]--;
+				if (indegrees[linkedListArray[u-1].get(v) - 1] == 0) {
+					q.add(linkedListArray[u-1].get(v));
+				}
+			}	
+		}
+		//System.out.println("i: " + i);
+		if (i != n) {
+			throw new IllegalArgumentException();
+		}
+		return a;
 	}
 	
 }
--- Program5/DiGraphTest.java
+++ Program5/DiGraphTest.java
@@ -4,8 +4,8 @@ public class DiGraphTest {
 
 	public static void main(String[] args) {
 		int vertices; 
-		String choice;
-		Scanner in = new Scanner(System.in);
+		String choice = "random";
+		Scanner in = new Scanner(System.in).useDelimiter("\n| "); // | 
 		System.out.println("Enter number of vertices");
 		vertices = in.nextInt();
 		DiGraph diGraph = new DiGraph(vertices);
@@ -17,49 +17,79 @@ public class DiGraphTest {
 		System.out.println("- vertex count (enter v)");
 		System.out.println("- print graph (enter p)");
 		System.out.println("- Quit (enter q)");
-		choice = in.next();
 		
-		while (choice != "q") {
+		in.nextLine();
+		choice = in.nextLine();
+
+		while (!choice.equals("q")) {
 			int from, to;
+				
+						
 			switch(choice) {
 				case ("a"):
 					System.out.println("Enter two vertices for edge to add.");
 					from = in.nextInt();
 					to = in.nextInt();
 					
-					diGraph.addEdge(from, to);
-					System.out.println("Edge (" + from + ", " + to + ") added.");
+					if (diGraph.addEdge(from, to) == true) {
+						System.out.println("Edge (" + from + ", " + to + ") added.");
+					} else {
+						System.out.println("Edge (" + from + ", " + to + ") is already in graph.");
+					}
+                                        in.nextLine();
 					
 					break;
 				case ("d"):
 					System.out.println("Enter two vertices for edge to delete.");
 					from = in.nextInt();
-					to = in.nextInt();
+                                        to = in.nextInt();
 					
 					diGraph.deleteEdge(from, to);
 					System.out.println("Edge (" + from + ", " + to + ") deleted.");
+                                        in.nextLine();
 					
 					break;
 				case ("e"):
 					System.out.println("Edge count is: " + diGraph.edgeCount());
 					
-					break;
+                                        break;
 				case ("v"):
 					System.out.println("Vertex count is: " + diGraph.vertexCount());
 					
 					break;
 				case ("p"):
+					System.out.println("The graph is the following:");
 					diGraph.print();
 					
 					break;
+				case ("t"):
+					try {
+                                            int[] topSort = diGraph.topSort();
+                                            System.out.println("The graph is the following:");
+                                            for (int i = 0; i < topSort.length; i++) {
+                                                    System.out.print(topSort[i]);
+                                                    if (i != topSort.length - 1) {
+                                                            System.out.print(", ");
+                                                    }
+                                            }
+                                            System.out.println();
+                                        } catch (IllegalArgumentException e) {
+                                            System.out.println("Error: graph is cyclic.");
+                                        }
+                                        break;
 				default:
-					// nothing
+					System.out.println("Invalid option.");
+                                        break;
 			}
-			System.out.println(choice);
-			if (choice != "q") {
-				choice = in.next();
-			}
+                        
+                        choice = in.nextLine();
+                        if (choice.length() > 1) {
+                            System.out.println("Invalid option.");
+                            choice = in.nextLine();
+                            
+                        }
 		}
+                System.out.println("Good bye.");
 		
 	}
 
